ee3306cd2509b32c18d57be112107d92d250c0a9
Michael Kolawole <mkolawole@coursera.org>
2021-04-24 02:59:54 -0700

    babel config

--- __tests__/add.spec.js
+++ __tests__/add.spec.js
@@ -1,12 +1,12 @@
-var Add = require('../add');
-var Substract = require('../subtract');
+import Add from '../add';
+import Substract from '../subtract';
 
 describe('TEST all used Functions', function(){
     it('The add function adds two numbers correctly', function(){
         expect(Add(1,2)).toEqual(3);
-    })
+    });
 
     it('The subtract function subtracts two numbers correctly', function(){
-        expect(Substract(1,2)).toEqual(3);
-    })
+        expect(Substract(1,2)).toEqual(-1);
+    });
 })
